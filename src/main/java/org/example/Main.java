7d3c131361a48d9212d907c7181146a1f2a6fd75
MohamedIbnKhaled <mohamedrobot1@gmail.com>
2025-01-26 16:15:27 +0200

    Add JSON utility methods for serialization and deserialization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.example.util.Json;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
     }
 }
--- src/main/java/org/example/util/Json.java
+++ src/main/java/org/example/util/Json.java
@@ -1,4 +1,53 @@
 package org.example.util;
 
+
+import com.fasterxml.jackson.databind.*;
+
+import java.io.IOException;
+
 public class Json {
+    private static ObjectMapper myObjectMapper= defaultObjectmapper();
+    private static ObjectMapper defaultObjectmapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+        return objectMapper;
+    }
+    public static JsonNode parse(String jsonSrc) throws IOException {
+        return myObjectMapper.readTree(jsonSrc);
+    }
+
+    public static <T> T fromJson(JsonNode node , Class<T> tClass) throws IOException{
+        //set the values of json node into the class property
+        //from json value to java object
+        return myObjectMapper.treeToValue(node,tClass);
+    }
+
+    public static JsonNode toJson (Object obj ){
+        //from java object to json tree
+        return myObjectMapper.valueToTree(obj);
+    }
+    public static String stringify(JsonNode node) throws IOException{
+        return generateJson(node,false);
+        //example
+        // {"name":"John","age":30}
+    }
+    public static String stringifyPretty(JsonNode node) throws IOException{
+        return generateJson(node,true);
+        //example
+        // {
+        //   "name": "John",
+        //   "age": 30
+       // }
+    }
+
+    private static String generateJson(Object object, boolean pretty) throws IOException{
+        ObjectWriter writer =  myObjectMapper.writer();
+        if (pretty){
+            writer = writer.with(SerializationFeature.INDENT_OUTPUT);
+        }
+        return writer.writeValueAsString(object);
+    }
+
+
+
 }
