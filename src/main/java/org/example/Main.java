7565583cd95a794035374a57f774ed36e65d8301
MohamedIbnKhaled <mohamedrobot1@gmail.com>
2025-01-26 21:19:44 +0200

    made method to load the config file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,19 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.example.config.Configuration;
+import org.example.config.ConfigurationManger;
 import org.example.util.Json;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        System.out.println("start testing the configManger ");
+        ConfigurationManger.getInstance().loadConfigFile("src\\main\\resources\\http.json");
+        Configuration conf = ConfigurationManger.getInstance().getCurrentConfiguratoin();
+        System.out.println("port = " + conf.getPort());
+
 
     }
 }
--- src/main/java/org/example/config/ConfigurationManger.java
+++ src/main/java/org/example/config/ConfigurationManger.java
@@ -1,5 +1,12 @@
 package org.example.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.example.util.Json;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManger {
     private static ConfigurationManger myConfigurationManger;
     private static Configuration myCurrentConfiguration;
@@ -12,10 +19,41 @@ public class ConfigurationManger {
         }
         return myConfigurationManger;
     }
-    public void loadConfigFile(String filePath){
 
+    public void loadConfigFile(String filePath) throws IOException {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(filePath);
+
+        }catch (FileNotFoundException e){
+            throw new FileNotFoundException("Didn't find the config file");
+        }
+        StringBuffer sBuffer  = new StringBuffer();
+        int i;
+        try {
+            while(( i  = fileReader.read())!=-1){
+                sBuffer.append((char)i);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        JsonNode conf = null;
+        try{
+            conf=Json.parse(sBuffer.toString());
+        }
+        catch (IOException e){
+            throw new RuntimeException("Error parsing the config file ");
+        }
+        try {
+            myCurrentConfiguration = Json.fromJson(conf,Configuration.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Error parsing the config file internal");
+        }
     }
-    public Configuration getCurrentConfiguratoin(){
+    public Configuration getCurrentConfiguratoin() throws Exception {
+        if(myCurrentConfiguration==null){
+            throw new Exception("no Current Config");
+        }
         return myCurrentConfiguration;
     }
 
