696bf15e62bb8cb2774080d5f1e469961cc990ff
MohamedIbnKhaled <mohamedrobot1@gmail.com>
2025-01-26 23:11:47 +0200

    Creating a start method to establish a connection between the server and the client

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,11 @@ package org.example;
 
 import org.example.config.Configuration;
 import org.example.config.ConfigurationManger;
+import org.example.server.Server;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("start testing the configManger ");
-        ConfigurationManger.getInstance().loadConfigFile("src\\main\\resources\\http.json");
-        Configuration conf = ConfigurationManger.getInstance().getCurrentConfiguration();
-        System.out.println("port = " + conf.getPort());
-
-
+        Server server = new Server();
+        server.start();
     }
 }
--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,7 +1,41 @@
 package org.example.server;
 
+import org.example.config.Configuration;
+import org.example.config.ConfigurationManger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    public void start() throws Exception {
+        ConfigurationManger.getInstance().loadConfigFile("src\\main\\resources\\http.json");
+        Configuration conf = ConfigurationManger.getInstance().getCurrentConfiguration();
+        System.out.println("port = " + conf.getPort());
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            String html = "<html><body><h1>Hello, World!</h1></body></html>";
+            int contentLength = html.length();
+            String httpResponse =
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: text/html\r\n" +
+                            "Content-Length: " + contentLength + "\r\n" +
+                            "\r\n" +
+                            html;
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(httpResponse.getBytes());
+            outputStream.flush();
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
